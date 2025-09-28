731ff7b7dde97d13bd2075e9d30a983e8f707839
loftyyyy <lofty08.business@gmail.com>
2025-09-28 19:36:43 +0800

    Almost done, never heard of destructor tho

--- projects/TicTacToe/src/components/Board.jsx
+++ projects/TicTacToe/src/components/Board.jsx
@@ -3,16 +3,59 @@ import Square from './square'
 import {useState} from "react";
 
 function Board(){
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
     const [count, setCount] = useState(0)
-    function handleClick(){
-        setCount(count + 1)
+    function handleClick(i){
+        const nextSquares = squares.slice();
+        if(count % 2 === 0 && nextSquares[i] == null){
+            nextSquares[i] = "X";
+        }else if(count %2 !== 0 && nextSquares[i] == null){
+            nextSquares[i] = "O";
+        }
+        setCount(count + 1);
+        setSquares(nextSquares);
+        checkWin(nextSquares)
+    }
+    function checkWin(squares){
+        console.log(squares);
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for(let line of lines){
+            const[a,b,c] = line;
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                console.log("Winner");
+            }
+            console.log("Not Yet");
+        }
     }
 
     return (
-        <div className="board-row">
-            <Square count={count} handleClick={handleClick}/>
-            <Square count={count} handleClick={handleClick}/>
-            <Square count={count} handleClick={handleClick}/>
+        <div>
+            <div className="board-row">
+                <Square value={squares[0]} handleClick={() => {handleClick(0)}}/>
+                <Square value={squares[1]} handleClick={() => {handleClick(1)}}/>
+                <Square value={squares[2]} handleClick={() => {handleClick(2)}}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} handleClick={() => {handleClick(3)}}/>
+                <Square value={squares[4]} handleClick={() => {handleClick(4)}}/>
+                <Square value={squares[5]} handleClick={() => {handleClick(5)}}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} handleClick={() => {handleClick(6)}}/>
+                <Square value={squares[7]} handleClick={() => {handleClick(7)}}/>
+                <Square value={squares[8]} handleClick={() => {handleClick(8)}}/>
+            </div>
         </div>
     )
 }
--- projects/TicTacToe/src/components/Square.jsx
+++ projects/TicTacToe/src/components/Square.jsx
@@ -1,19 +1,8 @@
 import './board.css'
-import {useState} from "react";
 
 function Square(props){
-    const [move, setMove] = useState(null)
-
-    function handleClick(){
-        if(props.count %2 === 0 ){
-            setMove("X")
-        }else{
-            setMove("O")
-        }
-    }
-
     return(
-        <button className="square" onClick={handleClick}>{move}</button>
+        <button className="square" onClick={props.handleClick}>{props.value}</button>
     )
 }
 
