20769eb9aa325fe3159f1cfaa5f05810745388c4
loftyyyy <lofty08.business@gmail.com>
2025-09-30 23:18:27 +0800

    Trying to implement history of the moves created

--- projects/TicTacToe/src/components/Game.jsx
+++ projects/TicTacToe/src/components/Game.jsx
@@ -1,6 +1,9 @@
 import Board from  './Board.jsx';
 import {useState} from "react";
 function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     function handleBoardStateFromChild(board){
